d1ce37ec2a1a29f4e614d859dcc4c46453e8de70
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-17 23:43:03 -0400

    Pulled song data from "Spotify Top 500 Most Streamed Songs of All Time"
    -Modified get_songs.py
    -Modified songs.csv
    -Create conda environment "spotifytest"

--- get_songs.py
+++ get_songs.py
@@ -11,18 +11,22 @@ def SpotifyClientCredentials():
             redirect_uri="https://localhost:8888/callback")
 
 
-tfuri="spotify:playlist:37i9dQZEVXbLRQDuF5jeBp" #the uri for spotify's "Top 50-USA" playlist
+
+tfuri="spotify:playlist:0JiVp7Z0pYKI8diUV6HJyQ" #the uri for spotify's "Top 500 Most Streamed Songs of All Time" playlist
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-tft=spotify.playlist_items(playlist_id=tfuri, market="US", limit=50) 
+tfts=[]
+
 songs = []
 major, minor = "Major", "Minor"
 header = ['Name', 'Key', 'Mode', 'Key Signature']
-for track in tft["items"]:
-    song_feat= spotify.audio_features(track['track']['href'][34:])
-    key=song_feat[0]['key']
-    mode=song_feat[0]['mode']
-    kam=notes[key] + (" Major" if mode == 1 else " Minor")
-    songs.append([track['track']['name'], str(key), str(mode), kam])
+for i in range(7):
+    tft=spotify.playlist_items(playlist_id=tfuri, market="US", offset=100*i)
+    for track in tft["items"]:
+        song_feat= spotify.audio_features(track['track']['href'][34:])
+        key=song_feat[0]['key']
+        mode=song_feat[0]['mode']
+        kam=notes[key] + (" Major" if mode == 1 else " Minor")
+        songs.append([track['track']['name'], str(key), str(mode), kam])
     #print("Name: " + track['track']['name'] + " Key: " + notes[key] + " " + (major if mode == 1 else minor))
 
 with open("songs.csv", 'w') as csvfile:
