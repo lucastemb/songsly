aa7899b6b200023de6847d820295d0b499c93304
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-20 10:04:22 -0400

    made notes.py obsolete by moving dict into get_songs.py

--- get_songs.py
+++ get_songs.py
@@ -10,17 +10,22 @@ def SpotifyClientCredentials():
             client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
             redirect_uri="https://localhost:8888/callback")
 
-
+#initializes mongodb database
 client=MongoClient("mongodb://localhost:27017/")
-db=client.spotifydata #database
-collection=db.songdata #like table in sql
+db=client.spotifydata #creates database 
+collection=db.songdata #creates collection that contains the songdata
+
+#idea: create collection for each user(?)
 
 tfuri="spotify:playlist:0JiVp7Z0pYKI8diUV6HJyQ" #the uri for spotify's "Top 500 Most Streamed Songs of All Time" playlist
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 tfts=[] #track features
+
+#song info nomenclature(?)
 major, minor = "Major", "Minor"
 header = ['Name', 'Key', 'Mode', 'Key Signature']
-posts=db.posts
+notes={0:'C', 1:'C♯/D♭', 2:'D', 3:'E♭', 4:'E', 5:'F', 6:'F♯/G♭', 7:'G', 8:'A♭', 9:'A', 10:'B♭', 11:'B/C♭'}
+
 for i in range(7):
     tft=spotify.playlist_items(playlist_id=tfuri, market="US", offset=100*i)
     for track in tft["items"]:
@@ -32,7 +37,7 @@ for i in range(7):
              "name": track['track']['name'],
              "key and mode": kam,
         }
-        post_id=posts.insert_one(post).inserted_id
+        post_id=collection.insert_one(post).inserted_id
         
 
 
