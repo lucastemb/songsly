dcf5d7a68f587f4d799838f507174f8a42334354
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-20 09:43:20 -0400

    added mongodb support

--- get_songs.py
+++ get_songs.py
@@ -2,7 +2,7 @@ import spotipy
 import os 
 from spotipy import SpotifyOAuth
 from notes import notes
-import csv
+from pymongo import MongoClient
 
 #returns spotify authentication
 def SpotifyClientCredentials():
@@ -11,29 +11,31 @@ def SpotifyClientCredentials():
             redirect_uri="https://localhost:8888/callback")
 
 
+client=MongoClient("mongodb://localhost:27017/")
+db=client.spotifydata #database
+collection=db.songdata #like table in sql
 
 tfuri="spotify:playlist:0JiVp7Z0pYKI8diUV6HJyQ" #the uri for spotify's "Top 500 Most Streamed Songs of All Time" playlist
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-tfts=[]
-
-songs = []
+tfts=[] #track features
 major, minor = "Major", "Minor"
 header = ['Name', 'Key', 'Mode', 'Key Signature']
+posts=db.posts
 for i in range(7):
     tft=spotify.playlist_items(playlist_id=tfuri, market="US", offset=100*i)
     for track in tft["items"]:
         song_feat= spotify.audio_features(track['track']['href'][34:])
         key=song_feat[0]['key']
         mode=song_feat[0]['mode']
-        kam=notes[key] + (" Major" if mode == 1 else " Minor")
-        songs.append([track['track']['name'], str(key), str(mode), kam])
-    #print("Name: " + track['track']['name'] + " Key: " + notes[key] + " " + (major if mode == 1 else minor))
+        kam=notes[key] + (" Major" if mode == 1 else " Minor") #key and mode
+        post={
+             "name": track['track']['name'],
+             "key and mode": kam,
+        }
+        post_id=posts.insert_one(post).inserted_id
+        
 
-with open("songs.csv", 'w') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(header)
-        for song in songs: 
-            csv_writer.writerow(song)
+
 
     
 
