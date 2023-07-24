ce1147b3415848b5337723026dd0d2cd7af68090
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-24 13:00:54 -0400

    added func to find top 3 artists

--- keygui/frontend/analyzePlaylist.py
+++ keygui/frontend/analyzePlaylist.py
@@ -16,14 +16,25 @@ notes={0:'C', 1:'C♯/D♭', 2:'D', 3:'E♭', 4:'E', 5:'F', 6:'F♯/G♭', 7:'G'
 class analyzePlaylist():
     def __init__(self, uri): 
         self.uri = uri
+        self.tracks=spotify.playlist_items(self.uri)
 
+    #for now this keeps in mind that 
     def keySorter(self):
-        tracks=spotify.playlist_items(self.uri)
         keys={}
-        for track in tracks["items"]:
+        for track in self.tracks["items"]:
             song_feat=spotify.audio_features(track['track']['uri'])
             key=song_feat[0]['key']
             mode=song_feat[0]['mode']
             kam=notes[key] + (" Minor" if mode == 0 else " Major")
             keys[kam]=1 + keys.get(kam, 0)
         return keys
+
+    def sortArtists(self):
+        artist_freq={}
+        for track in self.tracks["items"]:
+            artist=track['track']['artists']
+            for i in artist:
+                artist_freq[i["name"]]=1 + artist_freq.get(i["name"],0)
+        return sorted(artist_freq.items(), key=lambda a: a[1], reverse=True)[0:3]
+
+    
