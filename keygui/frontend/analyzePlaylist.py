542c966e2338494aa55b55339b37e6e94404819b
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-24 16:26:15 -0400

    added bpm sorting function
    note: i'm a little confused about how this will work in the end (kinda just figuring it out as i go).
    
    idea(?): object of this class will be created, and once daily, all of the appropriate functions will be called, and the data will be stored in the mongodb database. from there, the website can access the database to display the information without needing to perform time-consuming operations every time the page is rendered.

--- keygui/frontend/analyzePlaylist.py
+++ keygui/frontend/analyzePlaylist.py
@@ -19,7 +19,7 @@ class analyzePlaylist():
         self.tracks=spotify.playlist_items(self.uri)
 
     #for now this keeps in mind that 
-    def keySorter(self):
+    def sortKeys(self):
         keys={}
         for track in self.tracks["items"]:
             song_feat=spotify.audio_features(track['track']['uri'])
@@ -46,3 +46,26 @@ class analyzePlaylist():
                 for genre in genres: 
                     genre_freq[genre] = 1 + genre_freq.get(genre, 0)
         return sorted(genre_freq.items(), key=lambda a: a[1], reverse=True)[0:3]
+
+    def sortBpm(self):
+        bpm_ranges={}
+        for track in self.tracks["items"]:
+            song_feat=spotify.audio_features(track['track']['uri'])
+            bpm=song_feat[0]['tempo']
+            if bpm > 200:
+                bpm_ranges["200-∞ bpm"]= 1 + bpm_ranges.get("200-∞ bpm", 0)
+            elif bpm > 168 and bpm <= 200: 
+                bpm_ranges["168-200 bpm"]= 1 + bpm_ranges.get("168-200 bpm", 0)
+            elif bpm > 120 and bpm <= 168: 
+                bpm_ranges["120-168 bpm"]= 1 + bpm_ranges.get("120-168 bpm", 0)
+            elif bpm > 108 and bpm <= 120: 
+                bpm_ranges["108-120 bpm"]= 1 + bpm_ranges.get("108-120 bpm", 0)
+            elif bpm > 76 and bpm <= 108: 
+                bpm_ranges["76-108 bpm"]= 1 + bpm_ranges.get("76-108 bpm", 0)
+            elif bpm > 66 and bpm <= 76: 
+                bpm_ranges["66-76 bpm"]= 1 + bpm_ranges.get("66-76 bpm", 0)
+            elif bpm > 60 and bpm <= 66: 
+                bpm_ranges["60-66 bpm"] = 1 + bpm_ranges.get("60-66 bpm", 0)
+            else: 
+                bpm_ranges["40-60 bpm"] = 1 + bpm_ranges.get("40-60 bpm", 0)
+        return sorted(bpm_ranges.items(), key=lambda a: int(a[0].split('-')[0]))
