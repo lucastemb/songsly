fbd7e7aaead39bc49c48694efa2fbe23415f547b
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-24 18:03:59 -0400

    adds time sig and mode to database

--- keygui/frontend/main.py
+++ keygui/frontend/main.py
@@ -15,7 +15,9 @@ post={
     "topGenres": billboard.topGenres(),
     "keyCounter": billboard.sortKeys(),
     "topArtists": billboard.topArtists(),
-    "bpmRangeCounter": billboard.sortBpm()      
+    "bpmRangeCounter": billboard.sortBpm(),
+    "keySignatureCounter": billboard.sortTimeSignature(),
+    "modeCounter": billboard.sortMode()
              
 }
 post_id=collection.insert_one(post).inserted_id
@@ -24,7 +26,10 @@ post_id=collection.insert_one(post).inserted_id
 sample output: 
 [('rap', 48), ('pop', 32), ('trap', 30)]
 {'F Major': 6, 'F♯/G♭ Major': 7, 'A♭ Major': 6, 'B/C♭ Major': 4, 'C Major': 8, 'E♭ Major': 1, 'A Major': 4, 'C♯/D♭ Minor': 6, 'D Major': 6, 'F Minor': 6, 'B/C♭ Minor': 3, 'E Major': 3, 'E Minor': 5, 'C♯/D♭ Major': 7, 'E♭ Minor': 2, 'D Minor': 5, 'A Minor': 4, 'F♯/G♭ Minor': 4, 'G Minor': 5, 'G Major': 3, 'B♭ Minor': 3, 'C Minor': 1, 'B♭ Major': 1}
+[('3/4', 13), ('4/4', 85), ('5/4', 2)]
 [('Lil Uzi Vert', 18), ('Peso Pluma', 11), ('Morgan Wallen', 8)]
-[('108-120 bpm', 10), ('120-168 bpm', 56), ('168-200 bpm', 9), ('66-76 bpm', 3), ('76-108 bpm', 21), ('>200 bpm', 1)]
+[('66-76 bpm', 3), ('76-108 bpm', 21), ('108-120 bpm', 10), ('120-168 bpm', 56), ('168-200 bpm', 9), ('200-∞ bpm', 1)]
+{' Major': 56, ' Minor': 44}
+
 
 '''
