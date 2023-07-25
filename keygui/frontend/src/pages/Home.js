f85961df7a98e836f923f3b74c68188c662a4518
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-25 11:16:27 -0400

    added variables for key, genre, and artists
    todo: incorporate the setVars func into fetchBillboardData. the additional useEffect is redundant.

--- keygui/frontend/src/pages/Home.js
+++ keygui/frontend/src/pages/Home.js
@@ -3,6 +3,12 @@ import {useEffect, useState} from 'react'
 
 const Home = () => {
     const [billboardData, setBillboardData] = useState(null)
+    const [keyCounter, setKeyCounter] = useState(null)
+    const [topGenres, setTopGenres] = useState(null)
+    const [topArtists, setTopArtists] = useState(null)
+    // const [bpmRangeCounter, setBpmRangeCounter] = useState(null)
+    // const [keySigantureCounter, setKeySignatureCounter] = useState(null)
+    // const [modeCounter, setModeCounter] = useState(null)
 
     useEffect(()=> {
         const fetchBillboardData = async () => {
@@ -16,14 +22,26 @@ const Home = () => {
         }
         fetchBillboardData()
     }, [])
+
+    useEffect(() => {
+        const setVars = async () => {
+            setKeyCounter((billboardData[0]["keyCounter"]))
+            setTopGenres((billboardData[0]["topGenres"]).map((genre)=> genre[0]))
+            setTopArtists((billboardData[0]["topArtists"]).map((artist)=> artist[0]))
+        }
+        setVars()
+    }, [billboardData])
+
     return (
         <div className="home">
             <div className="songs">
-                {billboardData && billboardData.map((data, index)=>(
+                {billboardData && billboardData.map((data)=>(
                     <div key={data._id}>
-                        <p> {data.topGenres} </p>
-                        <p> {data.topArtists} </p>
-                        <p> {data.bpmRangeCounter} </p>
+                        <p> {topGenres} </p>
+                        <p> {topArtists} </p>
+                        <p> {JSON.stringify(data.bpmRangeCounter)} </p>
+                        <p> {JSON.stringify(keyCounter)} </p>
+                        <p> {JSON.stringify(data.modeCounter)} </p>
                     </div> 
 
                 ))}
