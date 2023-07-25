3a02c925e414ec6b9e0c8e662429e30bd14f15e4
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-25 12:02:44 -0400

    added top genres and top artists to site
    todo:
    -create charts from the remaining data

--- keygui/frontend/src/pages/Home.js
+++ keygui/frontend/src/pages/Home.js
@@ -2,11 +2,10 @@ import {useEffect, useState} from 'react'
 
 
 const Home = () => {
-    const [billboardData, setBillboardData] = useState(null)
-    const [keyCounter, setKeyCounter] = useState(null)
     const [topGenres, setTopGenres] = useState(null)
     const [topArtists, setTopArtists] = useState(null)
-    // const [bpmRangeCounter, setBpmRangeCounter] = useState(null)
+    //const [keyCounter, setKeyCounter] = useState(null)
+    //const [bpmRangeCounter, setBpmRangeCounter] = useState(null)
     // const [keySigantureCounter, setKeySignatureCounter] = useState(null)
     // const [modeCounter, setModeCounter] = useState(null)
 
@@ -16,35 +15,30 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok){
-                setBillboardData(json)
-                console.log(json)
+                //setKeyCounter((json[0]["keyCounter"]))
+                setTopGenres((json[0]["topGenres"])) //extract top genres only, not their frequency 
+                setTopArtists((json[0]["topArtists"])) //extract top artists only, not their frequency
             }
         }
         fetchBillboardData()
     }, [])
 
-    useEffect(() => {
-        const setVars = async () => {
-            setKeyCounter((billboardData[0]["keyCounter"]))
-            setTopGenres((billboardData[0]["topGenres"]).map((genre)=> genre[0]))
-            setTopArtists((billboardData[0]["topArtists"]).map((artist)=> artist[0]))
-        }
-        setVars()
-    }, [billboardData])
+
 
     return (
         <div className="home">
             <div className="songs">
-                {billboardData && billboardData.map((data)=>(
-                    <div key={data._id}>
-                        <p> {topGenres} </p>
-                        <p> {topArtists} </p>
-                        <p> {JSON.stringify(data.bpmRangeCounter)} </p>
-                        <p> {JSON.stringify(keyCounter)} </p>
-                        <p> {JSON.stringify(data.modeCounter)} </p>
+                    <div>
+                        <div> 
+                            {/* Need to include topGenres && because it ensures the value is not null */}
+                            {topGenres && topGenres.map((genre, item)=>(
+                                <p> {item+1}). {genre[0]} </p>))}
+                        </div>
+                        <div> 
+                            {topArtists && topArtists.map((artist, item)=>(
+                                <p> {item+1}). {artist[0]} </p>))}
+                        </div>
                     </div> 
-
-                ))}
             </div>
         </div>
     )
