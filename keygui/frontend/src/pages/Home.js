fa13d208b5e37bd86dd8598f9b35f1bebea8dac8
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-30 14:27:00 -0400

    changed layout on small screens

--- keygui/frontend/src/pages/Home.js
+++ keygui/frontend/src/pages/Home.js
@@ -9,6 +9,13 @@ const Home = () => {
     const [bpmRangeCounter, setBpmRangeCounter] = useState(null)
     const [keySignatureCounter, setKeySignatureCounter] = useState(null)
     const [modeCounter, setModeCounter] = useState(null)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    const graphInit = (windowWidth > 768 ? ["33vw", "50vh", true] : ["85vw", "50vh", false])
+    const [graphSize, setGraphSize] = useState(graphInit)
+
+    const detectWidth = () =>{
+        setWindowWidth(window.innerWidth)
+    }
 
     useEffect(()=> {
         const fetchBillboardData = async () => {
@@ -28,75 +35,90 @@ const Home = () => {
         fetchBillboardData()
     }, [])
 
+
+    useEffect(()=> {
+        window.addEventListener('resize', detectWidth)
+        return()=> {
+            window.removeEventListener('resize', detectWidth)
+            if (windowWidth < 768){
+                setGraphSize(["85vw", "50vh", false])
+            }
+            else {
+                setGraphSize(["33vw","50vh", true])
+            }
+        }
+        
+    }, [windowWidth])
+
     return (
         <div className="home">
-            <div className="songs">
-                    <div className="grid grid-rows-2">
-                        <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center">
-                        <div className="col-span-1 overflow-hidden">
-                            {keyCounter && 
-                                <Plot useResizeHandler={true} style={{width: "33vw",height:"50vh"}} layout={{title: "Keys"}} data={[
-                                    {
-                                        x: Object.keys(keyCounter), 
-                                        y: Object.values(keyCounter),
-                                        type: 'bar'
-                                    }
-                                ]} />
-                            }
-                        </div>
-                        <div className="grid items-center col-span-1 overflow-hidden"> 
-                            <div>
-                            {topArtists && topArtists.map((artist, item)=>(
-                                <p className="text-2xl font-medium m-3" key={item}> {item+1}). {artist[0]} </p>))}
-                            </div> 
-                        </div>
-                        <div className="col-span-1 overflow-hidden">
-                            {modeCounter && 
-                                <Plot useResizeHandler={true} style={{width: "33vw",height:"50vh"}} layout={{title: "Modes"}} data={[
-                                    {
-                                        label: Object.keys(modeCounter), 
-                                        values: Object.values(modeCounter),
-                                        type: 'pie'
-                                    }
-                                ]}/>
-                            }
-                        </div>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center overflow-hidden gap-y-4"> 
-                        <div className="grid items-center col-span-1"> 
-                            <div>
-                            {/* Need to include topGenres && because it ensures the value is not null */}
-                            {topGenres && topGenres.map((genre, item)=>(
-                                <p className="text-2xl font-medium m-3" key ={item}> {item+1}). {genre[0].charAt(0).toUpperCase() + genre[0].slice(1)} </p>))}
-                            </div>
-                        </div>
+        <div className="songs">
+                <div className="grid grid-rows-2">
+                    <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center">
+                    <div className="col-span-1 overflow-hidden">
+                        {keyCounter && 
+                            <Plot useResizeHandler={graphSize[2]} style={{width: graphSize[0], height: graphSize[1]}} layout={{title: "Keys"}} data={[
+                                {
+                                    x: Object.keys(keyCounter), 
+                                    y: Object.values(keyCounter),
+                                    type: 'bar'
+                                }
+                            ]} />
+                        }
+                    </div>
+                    <div className="grid items-center col-span-1 overflow-hidden"> 
                         <div>
-                            {bpmRangeCounter && 
-                                <Plot useResizeHandler={true} style={{width: "33vw",height:"50vh"}} data={[
-                                    {
-                                        labels: bpmRangeCounter.map(bpm => bpm[0]), 
-                                        values: bpmRangeCounter.map(bpm => bpm[1]),
-                                        type: 'pie',
-                                    }
-                                ]}/>
-                            }
-                        </div>
+                        {topArtists && topArtists.map((artist, item)=>(
+                            <p className="text-2xl font-medium m-3" key={item}> {item+1}). {artist[0]} </p>))}
+                        </div> 
+                    </div>
+                    <div className="col-span-1 overflow-hidden">
+                        {modeCounter && 
+                            <Plot useResizeHandler={graphSize[2]} style={{width: graphSize[0], height: graphSize[1]}} layout={{title: "Modes"}} data={[
+                                {
+                                    label: Object.keys(modeCounter), 
+                                    values: Object.values(modeCounter),
+                                    type: 'pie'
+                                }
+                            ]}/>
+                        }
+                    </div>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center overflow-hidden gap-y-4"> 
+                    <div className="grid items-center col-span-1"> 
                         <div>
-                            {keySignatureCounter && 
-                                <Plot useResizeHandler={true} style={{width: "33vw",height:"50vh"}} data={[
-                                    {
-                                        x: keySignatureCounter.map(ks => ks[0]), 
-                                        y: keySignatureCounter.map(ks => ks[1]),
-                                        type: 'bar',
-                                    }
-                                ]}/>
-                            }
+                        {/* Need to include topGenres && because it ensures the value is not null */}
+                        {topGenres && topGenres.map((genre, item)=>(
+                            <p className="text-2xl font-medium m-3" key ={item}> {item+1}). {genre[0].charAt(0).toUpperCase() + genre[0].slice(1)} </p>))}
                         </div>
-                        </div>
-                        
-                    </div> 
-            </div>
+                    </div>
+                    <div>
+                        {bpmRangeCounter && 
+                            <Plot useResizeHandler={graphSize[2]} style={{width: graphSize[0], height: graphSize[1]}} data={[
+                                {
+                                    labels: bpmRangeCounter.map(bpm => bpm[0]), 
+                                    values: bpmRangeCounter.map(bpm => bpm[1]),
+                                    type: 'pie',
+                                }
+                            ]}/>
+                        }
+                    </div>
+                    <div>
+                        {keySignatureCounter && 
+                            <Plot useResizeHandler={graphSize[2]} style={{width: graphSize[0], height: graphSize[1]}} data={[
+                                {
+                                    x: keySignatureCounter.map(ks => ks[0]), 
+                                    y: keySignatureCounter.map(ks => ks[1]),
+                                    type: 'bar',
+                                }
+                            ]}/>
+                        }
+                    </div>
+                    </div>
+                    
+                </div> 
         </div>
+    </div>
     )
 }
 
