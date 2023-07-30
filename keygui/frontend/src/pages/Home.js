78dcb0f692e9b3329f48b799a2ddb77f06e0c658
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-30 15:55:19 -0400

    added spotify color scheme to pie charts

--- keygui/frontend/src/pages/Home.js
+++ keygui/frontend/src/pages/Home.js
@@ -57,7 +57,20 @@ const Home = () => {
                     <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center">
                     <div className="col-span-1 overflow-hidden">
                         {keyCounter && 
-                            <Plot config={{'displayModeBar': false, 'staticPlot': true}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} layout={{title: "Keys", paper_bgcolor:'rgba(0,0,0,0)', plot_bgcolor:'rgba(0,0,0,0)'}} data={[
+                            <Plot className="font-semibold tracking-wide" config={{'displayModeBar': false}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} 
+                            layout={
+                                {title: "<b> Keys </b>", 
+                                paper_bgcolor:'rgba(0,0,0,0)', 
+                                plot_bgcolor:'rgba(0,0,0,0)',
+                                font:{
+                                    family: 'Arial',
+                                    color: 'white',
+                                },
+                                
+    
+                            }
+                            } 
+                            data={[
                                 {
                                     x: Object.keys(keyCounter), 
                                     y: Object.values(keyCounter),
@@ -74,7 +87,19 @@ const Home = () => {
                     </div>
                     <div className="col-span-1 overflow-hidden">
                         {modeCounter && 
-                            <Plot config={{'displayModeBar': false, 'staticPlot': true}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} layout={{title: "Modes", paper_bgcolor:'rgba(0,0,0,0)', plot_bgcolor:'rgba(0,0,0,0)'}} data={[
+                            <Plot className="font-semibold tracking-wide" config={{'displayModeBar': false}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} 
+                            layout={
+                                {title: "<b> Modes </b>", 
+                                paper_bgcolor:'rgba(0,0,0,0)', 
+                                plot_bgcolor:'rgba(0,0,0,0)',
+                                font:{
+                                    family: 'Arial',
+                                    color: 'white',
+                                },
+                                colorway: ['#1DB954', '#6FF29D','#26F06D','#347049','#1EBD56']   
+                            }
+                            } 
+                            data={[
                                 {
                                     label: Object.keys(modeCounter), 
                                     values: Object.values(modeCounter),
@@ -94,7 +119,18 @@ const Home = () => {
                     </div>
                     <div>
                         {bpmRangeCounter && 
-                            <Plot layout={{paper_bgcolor:'rgba(0,0,0,0)', plot_bgcolor:'rgba(0,0,0,0)'}} config={{'displayModeBar': false, 'staticPlot': true}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} data={[
+                            <Plot className="font-semibold tracking-wide" 
+                            layout={
+                                {paper_bgcolor:'rgba(0,0,0,0)', 
+                                plot_bgcolor:'rgba(0,0,0,0)', 
+                                font:{
+                                family: 'Arial',
+                                color: 'white',
+                                },
+                                colorway: ['#1DB954', '#6FF29D','#26F06D','#347049','#1EBD56']   
+                                }
+                            } 
+                            config={{'displayModeBar': false}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} data={[
                                 {
                                     labels: bpmRangeCounter.map(bpm => bpm[0]), 
                                     values: bpmRangeCounter.map(bpm => bpm[1]),
@@ -105,7 +141,16 @@ const Home = () => {
                     </div>
                     <div>
                         {keySignatureCounter && 
-                            <Plot layout={{paper_bgcolor:'rgba(0,0,0,0)', plot_bgcolor:'rgba(0,0,0,0)'}} config={{'displayModeBar': false, 'staticPlot': true}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} data={[
+                            <Plot className="font-semibold tracking-wide" layout={
+                                {
+                                    paper_bgcolor:'rgba(0,0,0,0)', 
+                                    plot_bgcolor:'rgba(0,0,0,0)', 
+                                    font:{
+                                    family: 'Arial',
+                                    color: 'white',
+                                    }
+                                }
+                            } config={{'displayModeBar': false}} useResizeHandler={true} style={{width: graphSize[0], height: graphSize[1]}} data={[
                                 {
                                     x: keySignatureCounter.map(ks => ks[0]), 
                                     y: keySignatureCounter.map(ks => ks[1]),
