c9cb1e2f1f7bb139efe035530d59cd35897635f0
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-08-02 16:26:52 -0400

    display track features

--- keygui/frontend/src/pages/Album.js
+++ keygui/frontend/src/pages/Album.js
@@ -1,13 +1,20 @@
 import {useEffect, useState} from 'react'
 
+
+
 const Album = ({album}) => {
+    console.log(album.features)
+
     return(
     <div>
+        
         <h2> {album.name} </h2>
         <div> 
+            
             {album.tracks && album.tracks.map((track, item)=>(
-                <p id={item}> {track.track_number}). {track.name} </p>
+                <p id={item}> {track.track_number}). {track.name} {album.features[item].key} {album.features[item].mode} {album.features[item].tempo} {album.features[item].time_signature}/4 {album.features[item].duration_ms} </p>
             ))}
+
         </div>
     </div>
     )
