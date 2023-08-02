9f613db392abba5813ff3c4859b0e1a7b96f6db1
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-08-02 12:33:20 -0400

    returns name of album on search

--- keygui/frontend/src/pages/AlbumAnalysis.js
+++ keygui/frontend/src/pages/AlbumAnalysis.js
@@ -1,11 +1,33 @@
 
 import React from 'react'
-//import { useAlbum } from 'react-spotify-api'
-//import {useEffect, useState} from 'react'
+import SpotifyWebApi from 'spotify-web-api-node'
+import {useEffect, useState} from 'react'
 
-const AlbumAnalysis = (props) =>{
+
+
+
+const AlbumAnalysis = (props) => {
+    const [album, setAlbum] = useState("")
+    const accessToken = ""
+    const spotifyApi = new SpotifyWebApi({
+        clientId: '',
+        clientSecret: '',
+        redirectUri: ''
+    })
+    const id = props.uri.substring(31,53) //playlist uri
+    useEffect(()=> {
+        spotifyApi.setAccessToken(accessToken)
+        spotifyApi.getAlbum(id).then((response)=>{
+            console.log(response);
+            setAlbum({
+                name: response.body.name
+            })
+        })
+    }, [])
+    
+    
     return(
-     <h1> {props.uri.substring(31,53)} </h1>
+        <h1> {album.name} </h1>
     )
 }
 
