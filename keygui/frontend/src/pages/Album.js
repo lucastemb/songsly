679f206fdd823d2186b83fe8d4798102a777e328
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-08-02 16:50:23 -0400

    all song features displaying accurately

--- keygui/frontend/src/pages/Album.js
+++ keygui/frontend/src/pages/Album.js
@@ -3,7 +3,15 @@ import {useEffect, useState} from 'react'
 
 
 const Album = ({album}) => {
-    console.log(album.features)
+    const keys = {0:'C', 1:'C♯/D♭', 2:'D', 3:'E♭', 4:'E', 5:'F', 6:'F♯/G♭', 7:'G', 8:'A♭', 9:'A', 10:'B♭', 11:'B/C♭'}
+    const mode = {0: "Minor", 1: "Major"}
+    
+    const ms_to_s = (ms) =>{
+        let min = Math.floor(ms/60000)
+        let sec= ((ms % 60000)/1000).toFixed(0)
+        return min + ":" + (sec < 10 ? '0' : '') + sec
+
+    }
 
     return(
     <div>
@@ -12,7 +20,11 @@ const Album = ({album}) => {
         <div> 
             
             {album.tracks && album.tracks.map((track, item)=>(
-                <p id={item}> {track.track_number}). {track.name} {album.features[item].key} {album.features[item].mode} {album.features[item].tempo} {album.features[item].time_signature}/4 {album.features[item].duration_ms} </p>
+                <div>
+                <p id={item}> {track.track_number}). {track.name}  {ms_to_s(album.features[item].duration_ms)} </p>
+                <p>{Math.round(album.features[item].tempo)} bpm {album.features[item].time_signature}/4 </p>
+                <p> {keys[album.features[item].key]} {mode[album.features[item].mode]} </p>
+                </div>
             ))}
 
         </div>
