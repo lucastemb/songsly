14e6542d4e7fd6a944a243e6b170f8118b1fbcbf
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-08-04 02:21:43 -0400

    updated album analysis

--- keygui/frontend/src/pages/AlbumAnalysis.js
+++ keygui/frontend/src/pages/AlbumAnalysis.js
@@ -2,32 +2,46 @@
 import React from 'react'
 import SpotifyWebApi from 'spotify-web-api-node'
 import {useEffect, useState} from 'react'
+import Album from './Album'
 
 
 
 
 const AlbumAnalysis = (props) => {
     const [album, setAlbum] = useState("")
+    const [features, setFeatures] = useState(null)
+    
     const accessToken = ""
     const spotifyApi = new SpotifyWebApi({
         clientId: '',
         clientSecret: '',
         redirectUri: ''
     })
-    const id = props.uri.substring(31,53) //playlist uri
+    const album_id = props.uri.substring(31,53) //playlist uri
     useEffect(()=> {
+        const ids = []
         spotifyApi.setAccessToken(accessToken)
-        spotifyApi.getAlbum(id).then((response)=>{
-            console.log(response);
-            setAlbum({
-                name: response.body.name
+        spotifyApi.getAlbum(album_id).then((response)=>{
+           (response.body.tracks.items.map((songs)=>{
+                ids.push(songs.id)
+            }))
+            spotifyApi.getAudioFeaturesForTracks(ids).then((res)=>{
+                console.log(res.body.audio_features)
+                setAlbum({
+                    name: response.body.name,
+                    images: response.body.images,
+                    tracks: response.body.tracks.items,
+                    features: res.body.audio_features
+                })
             })
+            
+            
+            
         })
     }, [])
     
-    
     return(
-        <h1> {album.name} </h1>
+        <Album album={album}/>
     )
 }
 
